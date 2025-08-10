e914ba7dd11ae7bb99052508f14da7066088ae4c
Emily Silkina <emily.silkin@gmail.com>
2025-08-09 19:45:40 -0700

    Added some screens

---
+++ PiggyBank/PiggyBank/GoalsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GoalsViewController.swift
+//  PiggyBank
+//
+//  Created by Emily Silkina on 8/9/25.
+//
+
+import SwiftUI
+
+struct GoalsViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GoalsViewController()
+}
